9f5a05101a34be95856af46e7cf4dc78793c7b5f
chajk811 <chajk811@gmail.com>
2021-11-01 20:56:55 +0900

    develop pagination with query string

--- src/components/Event/EventList.js
+++ src/components/Event/EventList.js
@@ -8,17 +8,26 @@ import './EventList.css';
 const EventList = ({ location }) => {
 	const queryData = qs.parse(location.search, { ignoreQueryPrefix: true });
 	const perPage = 2;
-	const page = parseInt(queryData.page);
+	const page = parseInt(queryData.page) || 1;
 	const [events, setEvents] = useState([]);
+	const [totalEvents, setTotalEvents] = useState(0);
+
+	// check total Page
+	const hasNextPage = () => {
+		const totalPage = Math.ceil(totalEvents / 2);
+		return page < totalPage;
+	};
 
 	useEffect(() => {
+		setEvents([]);
 		getEvents(perPage, page)
 			.then(response => {
 				const { data } = response;
 				setEvents(data);
+				setTotalEvents(response.headers['x-total-count']);
 			})
 			.catch(error => console.log('error: ', error));
-	}, []);
+	}, [page]);
 
 	return (
 		<div className="events">
@@ -28,6 +37,19 @@ const EventList = ({ location }) => {
 					<EventCard event={event} />
 				</Link>
 			))}
+
+			<div className="pagination">
+				{page !== 1 && (
+					<Link to={`/event?_limit=${perPage}&page=${page - 1}`} id="page-prev" rel="prev">
+						&#60; Previous
+					</Link>
+				)}
+				{hasNextPage() && (
+					<Link to={`/event?_limit=${perPage}&page=${page + 1}`} id="page-next" rel="next">
+						Next &#62;
+					</Link>
+				)}
+			</div>
 		</div>
 	);
 };
